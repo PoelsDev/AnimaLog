f0ed8f949368a87b7f1c31da5dbc367d9f784f26
Thibault Poels <31651240+ThibaultP-immalle@users.noreply.github.com>
2019-03-21 15:30:55 +0100

    user login work in progress

--- Web_app/main.py
+++ Web_app/main.py
@@ -1,13 +1,29 @@
 from flask import *
+from flask_login import UserMixin, login_user, logout_user, login_manager, login_required
 
 app = Flask(__name__)
+loginManager = login_manager()
+loginManager.init_app(app)
+loginManager.login_view = "home"
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+
+@login_manager.user_loader
+def load_user(id):
+    return User(id)
+
+testuser = "test@test.com"
+testpassword = "test"
+
 @app.route("/home", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         email = request.form["email"]
-        print(email)
         password = request.form["password"]
-        print(password)
+        if email == testuser and password == testpassword:
+            login_user()
     return render_template("index.html")
 
 @app.route("/")
